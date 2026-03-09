12e956f604887b22901a1cf5efc1abc581974860
Kindly Frostbite <kindlyfr@yc-c6.ufa.21-school.ru>
2026-03-09 18:12:55 +0500

    Initial Spring Boot project from Spring Initializr

---
+++ src/main/java/com/pkms/PkmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pkms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkms/PkmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
